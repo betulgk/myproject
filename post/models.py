536d7661d93b310334e9a89b05a0465dd0cadd5c
Betul Gokbel <gokbel.betul@metu.edu.tr>
2018-07-15 13:19:28 +0300

    Last changes (15.07.18)

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class post(models.Model):
     author = models.CharField(max_length=200)
+    post_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 
