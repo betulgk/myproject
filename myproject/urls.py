91e2b28cd83d8eafcd164bd7b999fdc657a65f1d
Betul Gokbel <gokbel.betul@metu.edu.tr>
2018-07-26 11:15:04 +0300

    26.07.18 Ajax OK

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,9 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from post.views import HomeView, JsonTestView
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('posts', JsonTestView.as_view(), name='jhome'),
+    path('posts/', JsonTestView.as_view(), name='jhome'),
+    url(r'^newestposts/(?P<pk>\d+)$', JsonTestView.as_view()),
+
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,10 @@
-from django.core.serializers import json
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 from .models import Post
 from django.contrib.auth.forms import *
 from .forms import PostForm
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse
 
 
 class HomeView(View):
@@ -16,24 +16,13 @@ class HomeView(View):
 
         return render(request, self.template_name, {'form': form, 'posts': posts})
 
-    def post(self, request):
-        form = PostForm(request.POST)
-
-        if request.method == 'POST':
-            post_name = request.POST.get('post_name')
-            post_content = request.POST.get('post_content')
-            post_author = User.objects.first()
-            add = Post(author=post_author, post_name=post_name, post_content=post_content)
-            add.save()
-            posts = Post.objects.all()
-            args = {'form': form, 'post_name ': post_name, 'post_content': post_content, 'posts': posts}
-
-        return render(request, 'home.html', args)
-
 
 class JsonTestView(View):
-    def get(self, request):
-        posts = Post.objects.all()
+    def get(self, request, **kwargs):
+        print(kwargs)
+        new_msg = request.GET;
+        print(new_msg)
+        posts = Post.objects.filter(id__gt=int(kwargs.get('pk'))).order_by('-pub_date')[:10]
 
         post_lists = []
 
@@ -44,10 +33,28 @@ class JsonTestView(View):
                 'name': post.post_name,
             })
 
-        data = {
-            'posts': post_lists
-        }
+        return JsonResponse(post_lists, safe=False)
 
-        args = {'posts': posts, 'data': data}
+    def post(self, request, *args, **kwargs):
+        # form = PostForm(request.POST)
 
-        return render(request, 'home.html', args)
+        """
+            post_name = request.POST.get('post_name')
+            post_content = request.POST.get('post_content')
+            post_author = User.objects.first()
+            add = Post(author=post_author, post_name=post_name, post_content=post_content)
+            add.save()
+            posts = Post.objects.all()
+            args = {'form': form, 'post_name ': post_name, 'post_content': post_content, 'posts': posts}
+
+            return render(request, 'home.html', args)"""
+
+        print(request.POST)
+        a = request.POST.get('post_content', None)
+        b = request.POST.get('post_name', None)
+
+        new_post = Post(author='', post_name=b, post_content=a)
+
+        new_post.save()
+
+        return JsonResponse({'saved': 'True'})
